2f31ff2e11a0216812f1ba3cd55e6d54078450b8
Solia <s.shuptar8@gmail.com>
2019-04-10 23:27:56 +0300

    changed

--- json_data.py
+++ json_data.py
@@ -5,19 +5,13 @@ def read_data(filename):
     with open(filename, 'r') as file:
         for line in file:
             line = line.strip().split(',')
-            station = line[3]
-            date = line[6]
-            time = line[7]
-            entrances = line[9]
-            exits = line[10]
-            coords = [line[11], line[12]]
             info = {
-                'station': station,
-                'date': date,
-                'time': time,
-                'entrances': entrances,
-                'exits': exits,
-                'coords': coords
+                'station': line[3],
+                'date': line[6],
+                'time': line[7],
+                'entrances': line[9],
+                'exits': line[10],
+                'coords': [line[11], line[12]]
             }
             yield info
 
